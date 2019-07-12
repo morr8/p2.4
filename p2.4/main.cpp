1f9c85b0b4c1c0dd1eba90601a512fe47d9eae2b
Sergio Campos <morrodogg8@gmail.com>
2019-07-12 13:07:17 -0700

    Initial Commit

---
+++ p2.4/main.cpp
@@ -0,0 +1,39 @@
+/*
+ ••• P2.4
+ Write a program that helps a person decide whether to buy a hybrid car. Your program’s inputs should be:
+ 
+ •The cost of a new car
+ 
+ •The estimated miles driven per year
+ •The estimated gas price
+ •The estimated resale value after 5 years
+ Compute the total cost of owning the car for 5 years. (For simplicity, we will not take the cost of financing into account.) Obtain realistic prices for a new and used hybrid and a comparable car from the Web. Run your program twice, using today’s gas price and 15,000 miles per year. Include pseudocode and the program runs with your assignment.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter cost of a new car: ";
+    float new_car_price;
+    cin >> new_car_price;
+    
+    cout << "Enter amount of miles driven per year: ";
+    float mpy; //miles per year
+    cin >> mpy;
+    
+    cout << "Enter estimated resale value after 5 years: ";
+    float resell_value;
+    cin >> resell_value;
+    
+    cout << "Enter todays gas price: ";
+    float gas_price;
+    cin >> gas_price;
+    
+    
+    
+    
+    
+}
